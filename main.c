62071be1721f1dcb7e253cd0653faea572b5c3f0
MathieuCaro <mathieucaro123@outlook.com>
2022-11-13 15:51:28 +0100

    ajout fonctions 3,4,5

--- main.c
+++ main.c
@@ -2,7 +2,15 @@
 
 int main(){
     int a=10,b=20;
-    swap_int(&a, &b);
-    add_two_int( &a,&b);
+    int tab[10]={1,2,3,4,0,2,13,54,0,7};
+    int tab2[10]={};
+    int tab3[10]={10,9,8,7,6,5,4,3,2,1};
+    //swap_int(&a, &b);
+    //add_two_int( &a,&b);
+    //equal_zero(tab,10);
+    //copy_array(tab,tab2,10);
+    //print_array(tab,10);
+    swap_array(tab,tab3,10);
+    return 0;
 }
 
--- pointeurs.c
+++ pointeurs.c
@@ -17,5 +17,64 @@ int add_two_int( int *a, int *b){
     *a = somme;
     printf("a = %d et b = %d\n", *a, *b);
     return 0;
+}
 
+int equal_zero(int tab[], int size){
+    int *pointeur;
+    pointeur=tab;
+    for(int i=0;i<size;i++){
+        if(*pointeur ==0){
+            printf("%d\n",i);
+        }
+        pointeur++;
+    }
+    return 0;
+}
+
+int copy_array(int tab[], int tab2[], int size){
+    int *pointeur1, *pointeur2;
+    pointeur1 = tab;
+    pointeur2 = tab2;
+    for(int i=0;i<size;i++){
+        pointeur2 = pointeur1;
+        printf("%d : %d\n", i,*pointeur2);
+        pointeur1++;
+        pointeur2++;
+    }
+}
+
+int print_array(int tab[], int size){
+    int *pointeur;
+    pointeur = tab;
+    for(int i=0;i<size;i++){
+        printf("%d : %d\n", i,*pointeur);
+        pointeur++;
+    }
+
+}
+
+int swap_array(int tab[], int tab2[], int size){
+    printf("Avant changement :\n");
+    printf("Tableau 1 :           Tableau 2\n");
+    int *pointeur1, *pointeur2;
+    int *a,*b,temp;
+    pointeur1 = tab;
+    pointeur2 = tab2;
+    for(int i=0;i<size;i++){
+        printf("%d : %d           %d :  %d \n", i,*pointeur1,i,*pointeur2);
+        pointeur1++;
+        pointeur2++;
+    }
+    printf("Apres changement :\n");
+    printf("Tableau 1 :           Tableau 2\n");
+    pointeur1 = tab;
+    pointeur2 = tab2;
+    for(int i=0;i<size;i++){
+        temp = *pointeur1;
+        *pointeur1 = *pointeur2;
+        *pointeur2 = temp;
+        printf("%d : %d           %d :  %d \n", i,*pointeur1,i,*pointeur2);
+        pointeur1++;
+        pointeur2++;
+    }
 }
